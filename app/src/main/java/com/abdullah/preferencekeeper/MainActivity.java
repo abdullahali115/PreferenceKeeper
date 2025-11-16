f8a4387b1e7fe0834a5263d5caa637cc84a1c8d1
Abdullah Ali <abdullahpasari123@gmail.com>
2025-11-17 00:53:21 +0500

    Initial commit

---
+++ app/src/main/java/com/abdullah/preferencekeeper/MainActivity.java
@@ -0,0 +1,77 @@
+package com.abdullah.preferencekeeper;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    TabLayout navBar;
+    Fragment frag;
+    FragmentManager manager;
+    FragmentTransaction transactor;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        frag = new ProfileView();
+        manager = getSupportFragmentManager();
+        transactor = manager.beginTransaction();
+        transactor.replace(R.id.frame, frag);
+        transactor.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        transactor.commit();
+        navBar = findViewById(R.id.menu);
+
+        navBar.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Fragment selectedFrag = null;
+                switch(tab.getPosition())
+                {
+                    case 0:
+                        selectedFrag = new ProfileView();
+                        Toast.makeText(MainActivity.this, "Profile", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 1:
+                        selectedFrag = new UserSettings();
+                        Toast.makeText(MainActivity.this, "Settings", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                if(selectedFrag != null)
+                {
+                    manager = getSupportFragmentManager();
+                    transactor = manager.beginTransaction();
+                    transactor.replace(R.id.frame, selectedFrag);
+                    transactor.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+                    transactor.commit();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+    }
+}
